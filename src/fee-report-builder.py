816e2ad9ffb5812add5a0adb78fc48628e06fc06
Andrea Ferracci <andrea.ferracci@gmail.com>
2025-02-03 10:33:33 +0100

    add SATY method for altri_io

--- src/fee-report-builder.py
+++ src/fee-report-builder.py
@@ -164,7 +164,7 @@ def get_gec_bundles():
 
         # altri_io and altri_wisp management
         #- AppIO - Carte PPAL MYBK BancomatPay
-        altri_io: bool = payment_type in ["PPAL", "BPAY"] and (touchpoint.lower() == "io" or touchpoint.lower() == "any")
+        altri_io: bool = payment_type in ["PPAL", "BPAY", "SATY"] and (touchpoint.lower() == "io" or touchpoint.lower() == "any")
         altri_wisp: bool = payment_type != "CP" and not conto and (touchpoint.lower() == "checkout" or touchpoint.lower() == "any")
 
         # getting configured polycy urls
